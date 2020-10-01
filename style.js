087e8c58dd18435bfb83d12daf8126535fbecc10
KHallam <kjhallam321@gmail.com>
2020-10-01 16:02:55 -0500

    added some pseudo code to the js

--- style.js
+++ style.js
@@ -1,9 +1,12 @@
 
 // Click the start button
-// Timer starts
 
----ARRAY of Questions (series of questions & answers)
-//I am presented with a question
+// Timer starts (for loop to countdown and remove time off the clock if answer is   incorrect) example: for(var countdown = 5; coundown >= 0; == countdown) { if (countdown) { console.log('T-Minus: ' + countdown);
+//} else {
+//  console.log('TimesUp!!!');}}
+
+//---ARRAY of Questions (series of questions & answers)
+//I am presented with a question 
 // answer questions (display correct or wrong)
 // time is subtracted - from the clock
 // all questions are answered or the timer reaches 0
