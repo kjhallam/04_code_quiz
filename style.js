61d892f03208d2151ef3595f8adc5c33c9bb430d
KHallam <kjhallam321@gmail.com>
2020-09-30 22:08:06 -0500

    added a comment section to begin evaluating script to js file

---
+++ style.js
@@ -0,0 +1,11 @@
+
+// Click the start button
+// Timer starts
+
+---ARRAY of Questions (series of questions & answers)
+//I am presented with a question
+// answer questions (display correct or wrong)
+// time is subtracted - from the clock
+// all questions are answered or the timer reaches 0
+// the game is over
+// THEN I can save my initials and score
